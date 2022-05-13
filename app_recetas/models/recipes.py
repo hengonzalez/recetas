d67b11c8603a7817d229ceea59dd3820dd42082a
Henry Gonzalez <co.henrygonzalez@gmail.com>
2022-05-13 16:04:34 -0500

    load project5

--- app_recetas/models/recipes.py
+++ app_recetas/models/recipes.py
@@ -1,4 +1,3 @@
-from unittest import result
 from app_recetas.config.mysqlconnection import connectToMySQL # import the connectToMySQL function from the application.config.mysqlconnection module
 from flask import flash
 import re
